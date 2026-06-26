9b5ecacd786315e10bff3f6379f235045cc69cc2
Sa Dararith <sadararith@brycencambodia.com.kh>
2026-06-26 09:16:19 +0700

    first commit

--- demo/index.php
+++ demo/index.php
@@ -5,8 +5,8 @@
     <title>Demo</title>
 </head>
 <body>
-    
-<?php
+    <!-- Vairable With Loop PHP -->
+<!-- <?php
    
     $books = [
         [
@@ -26,18 +26,60 @@
 ];
 
 
-?>
+?> 
 
 
 <ul>
     <?php foreach ($books as $book) : ?>
         <li>
             <a href="<?= $book['purchaseurl'] ?>">
-                <?= $book['name'];  ?>
+            <?= $book['name'] ?> (<?= $book['release'] ?>)            </a>
+        </li>
+    <?php endforeach; ?>
+</ul>  -->
+
+<!-- Function and Filtering PHP -->
+
+<?php
+    $books = [
+        [
+            'name' => 'Do Android Dream of Electric Sheep',
+            'author' => 'Phillip',
+            'release' => '1968',
+            'purchaseurl' => 'http://example.com'
+        ],
+        [
+            'name' => 'Dream of Electric Sheep',
+            'author' => 'PhillipJose',
+            'release' => '2000',
+            'purchaseurl' => 'http://example.com'
+        ],
+    ];
+
+    function filterbyAuthor($books, $author) {
+        $filterBooks = [];
+
+        foreach ($books as $book) {
+            if ($book['author'] === $author) {
+                $filterBooks[] = $book;
+            }
+        }
+        return $filterBooks;
+    }
+?>
+
+<ul>
+    <?php foreach (filterbyAuthor($books, 'Phillip') as $book) : ?>
+        <li>
+            <a href="<?= $book['purchaseurl'] ?>">
+                <?= $book['name'] ?> (<?= $book['release'] ?>) - By <?= $book['author'] ?>
             </a>
         </li>
     <?php endforeach; ?>
 </ul>
-    
+
+  
+
+
 </body>
 </html>
